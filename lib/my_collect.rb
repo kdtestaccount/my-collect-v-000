b00f199935b26221a189ffa7cf24a952f1834913
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 16:04:57 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(array)
   collection = []
   if array != []
     while i < array.length
-      collection << yield(array[i])
+      collection << yield(array[i].split(" ").first.upcase)
     end
   end
   collection
